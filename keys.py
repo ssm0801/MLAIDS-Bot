c481373c68cd4c5e5b770fbed557eed26dce4dda
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-09-24 23:28:42 +0530

    Uploaded

---
+++ keys.py
@@ -0,0 +1,9 @@
+# for twitter API you need to create account on https://developer.twitter.com/en and then you will get below keys
+
+API_KEY = "XXXX"
+API_SECRET_KEY = "XXXX"
+
+BEARER_TOKEN = "XXXX"
+
+ACCESS_TOKEN = "XXXX"
+SECRET_ACCESS_TOKEN = "XXXX"
